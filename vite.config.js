0e60b028a32a04247bbfc80931ee21ba887de8b5
Davidlsy <1447797934@qq.com>
2026-07-26 20:40:10 +0800

    修改vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
         target: 'http://localhost:8000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
+      },
+      '/mock': {
+        target: 'http://localhost:8000',
+        changeOrigin: true
       }
     }
   },
